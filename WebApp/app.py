a2b4958edff096ab482517691b95fefaaefdd5dd
Shirvan Hashimov <71458919+Shirvan1992@users.noreply.github.com>
2022-01-29 02:54:14 +0100

    update database and app

--- WebApp/app.py
+++ WebApp/app.py
@@ -5,23 +5,32 @@ from pickle import FALSE
 from urllib import request
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-from algorithm import solve_knapsack_problem
+from datetime import date, datetime
+from algorithm import check_possibility, solve_knapsack_problem
 import uuid
+import js2py
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test2.db'
 db = SQLAlchemy(app)
 
+# Create Database Tables
 class User_Entries(db.Model):
     id = db.Column(db.String(200), primary_key=True)
     doctor = db.Column(db.String(200), nullable= False)
-    operation_date = db.Column(db.String(200), nullable=False)
+    operation_date = db.Column(db.DateTime, nullable=False)
     department_name = db.Column(db.String(200), nullable=False)
-    department_capacity = db.Column(db.Integer, nullable=False)
     operation_duration  = db.Column(db.Integer, nullable= False)
     operation_urgency = db.Column(db.Integer, nullable=False)
+
+class Department_Info(db.Model):
+    id = db.Column(db.String(200), primary_key=True)
+    department_name = db.Column(db.String(200), nullable=False)
+    department_capacity = db.Column(db.String(200), nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+
+
 
     def __repr__(self):
         return '<User entry %r is created>' %self.id
@@ -30,30 +39,48 @@ class User_Entries(db.Model):
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        op_date = request.form['op_date']
-        dep_name = request.form['dep_name']
-        dep_capacity = request.form.get('dep_capacity')
-        op_duration = request.form['op_duration']
-        op_urgency = request.form['op_urgency']
+        if request.form['submit_btn'] == 'submit_form':
+            op_date = request.form['op_date']
+            dep_name = request.form['dep_name']
+            doctor_name = request.form['doctor_name']
+            op_duration = request.form['op_duration']
+            op_urgency = request.form['op_urgency']
 
-        ####
-        new_user_entry = User_Entries(
-                        id=str(dep_name + '-' + str(uuid.uuid4())), 
-                        doctor='John Doe', 
-                        operation_date=op_date, 
-                        department_name=dep_name,
-                        department_capacity=int(dep_capacity), 
-                        operation_duration=int(op_duration),
-                        operation_urgency=int(op_urgency)
-                        )
+            ####
+            new_user_entry = User_Entries(
+                            id=str(dep_name + '-' + str(uuid.uuid4())), 
+                            doctor=doctor_name, 
+                            operation_date=datetime.strptime(op_date, '%m/%d/%Y').date(), 
+                            department_name=dep_name,
+                            operation_duration=int(op_duration),
+                            operation_urgency=int(op_urgency)
+                            )
 
-        try:
-            db.session.add(new_user_entry)
-            db.session.commit()
-            return redirect('/')
-        except:
-            print('There was an issue adding new user entry')
-            raise 
+            possible_to_add = check_possibility()
+            try:
+                db.session.add(new_user_entry)
+                db.session.commit()
+                
+                if possible_to_add != 'possible':
+                    user_entry_to_delete = User_Entries.query.get_or_404(new_user_entry.id)
+                    try:
+                        db.session.delete(user_entry_to_delete)
+                        db.session.commit()
+                    except:
+                        return 'There was a problem deleting that'                
+                
+                    return render_template('alert.html')
+                return redirect('/')
+            except:
+                print('There was an issue adding new user entry')
+                raise 
+        
+        elif request.form['submit_btn'] == 'filter':
+            filtered_date = datetime.strptime(request.form['filter_date'], '%m/%d/%Y')
+            print(filtered_date)
+            filtered_entries = User_Entries.query.filter(User_Entries.operation_date==filtered_date).all()
+            print(f'filtered entries:{filtered_entries}')
+            return render_template('index.html', tasks=filtered_entries)
     else:
         entries = User_Entries.query.order_by(User_Entries.operation_date).all()
         return render_template('index.html', tasks=entries)
@@ -62,7 +89,6 @@ def index():
 @app.route('/delete/<id>')
 def delete(id):
     user_entry_to_delete = User_Entries.query.get_or_404(id)
-
     try:
         db.session.delete(user_entry_to_delete)
         db.session.commit()
@@ -70,10 +96,40 @@ def delete(id):
     except:
         return 'There was a problem deleting that'
 
-@app.route('/admin', methods=['GET'])
+
+@app.route('/admin', methods=['GET', 'POST'])
 def show_tables():
-    entries = User_Entries.query.order_by(User_Entries.operation_date).all()
-    return render_template('tables.html', tasks=entries)
+    if request.method == 'POST':
+        date = request.form['date']
+        dep_name = request.form['dep_name']
+        dep_capacity = request.form['dep_capacity']
+        
+        new_department_info = Department_Info(
+                id=str(dep_name + '-' + str(uuid.uuid4())),
+                date=datetime.strptime(date, '%m/%d/%Y').date(), 
+                department_name=dep_name,
+                department_capacity=int(dep_capacity), 
+                )
+        try:
+            db.session.add(new_department_info)
+            db.session.commit()
+            return redirect('/admin')
+        except:
+            print('There was an issue adding new user entry')
+            raise
+    else:
+        department_info = Department_Info.query.order_by(Department_Info.date).all()
+        return render_template('admin.html', departments=department_info)
+
+@app.route('/admin/delete/<id>')
+def admin_delete(id):
+    department_info_to_delete = Department_Info.query.get_or_404(id)
+    try:
+        db.session.delete(department_info_to_delete)
+        db.session.commit()
+        return redirect('/admin')
+    except:
+        return 'There was a problem deleting that'
 
 @app.route('/result', methods=['GET'])
 def optimize():
@@ -83,3 +139,4 @@ def optimize():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
